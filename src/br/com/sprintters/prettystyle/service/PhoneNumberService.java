32be419a8908c3525bc213b671e2ec052341e892
1garo <alevardai@hotmail.com>
2020-03-20 22:29:57 -0300

    feature: criando classe service da tabela phone_number

---
+++ src/br/com/sprintters/prettystyle/service/PhoneNumberService.java
@@ -0,0 +1,34 @@
+package br.com.sprintters.prettystyle.service;
+
+import br.com.sprintters.prettystyle.model.PhoneNumber;
+import br.com.sprintters.prettystyle.DAO.PhoneNumberDAO;
+
+import java.util.ArrayList;
+
+public class PhoneNumberService {
+    PhoneNumberDAO dao;
+
+    public PhoneNumberService() {
+        dao = new PhoneNumberDAO();
+    }
+
+    public void create(PhoneNumber phoneNumber) {
+        dao.create(phoneNumber);
+    }
+
+    public void update(PhoneNumber phoneNumber) {
+        dao.update(phoneNumber);
+    }
+
+    public void delete(PhoneNumber phoneNumber) {
+        dao.delete(phoneNumber);
+    }
+
+    public ArrayList<PhoneNumber> list(int id) {
+        return dao.list(id);
+    }
+
+    public PhoneNumber find(int id) {
+        return dao.find(id);
+    }
+}
