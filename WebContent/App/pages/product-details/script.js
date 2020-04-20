64ed9865fd6381dab55354865a6625100a60a011
Bruno Futema <bruno.futema@outlook.com>
2020-04-20 19:05:46 -0300

    feature: refatoração para front command toda a parte de visualização de produto detalhado

--- WebContent/App/pages/product-details/script.js
+++ WebContent/App/pages/product-details/script.js
@@ -1,57 +1,63 @@
-//function likeEvent(event){
-//	if (event){
-//		var r = document.getElementById("like");
-//		console.log(r.value);
-//		handleLogin(r.value);
-//	}
-//}
-
-
-$(document).ready(function () {
-	$('form[name="client-likes"]').validate({
-		submitHandler: function (form) {
-			handleLogin(form);
-		},
+function AlertaSucesso(data) {
+	swal({
+		title: 'Sucesso!',
+		text: data.message,
+		type: 'success',
 	});
-});
+};
 
-//function AlertaSucesso(data) {
-//	swal({
-//		title: 'Sucesso!',
-//		text: data.message,
-//		type: 'success',
-//	});
-//};
-//
-//function AlertaErro(data) {
-//	swal({
-//		title: 'Erro!',
-//		text: data.message,
-//		type: 'error',
-//	});
-//};
+function AlertaErro(data) {
+	swal({
+		title: 'Erro!',
+		text: data.message,
+		type: 'error',
+	});
+};
 
-function handleLogin(form) {
+function handleLikeDislike(div, id) {
+	let idUser = sessionStorage.getItem('id_user');
+	let userToken = sessionStorage.getItem('token');
+
+	if ($(div).hasClass('like')) {
+		$.ajax({
+			url: '/PrettyStyle/controller.do?path=favorites&command=CreateFavorite&json=true&id_user=' + idUser + '&token=' + userToken + '&id_product=' + id, type: 'POST',
+			success: function (data) {
+				if (data.success) {
+					AlertaSucesso(data);
 	
-	$.ajax({
-		url: '/PrettyStyle/likes', type: 'POST', data: $(form).serialize(),
-		success: function (data) {
-			if (data.success) {
-				sessionStorage.setItem('token', 'Bearer ' + data.token);
-				
-//				AlertaSucesso(data);
-				console.log(data);
-			} else {
-				sessionStorage.setItem('token', null);
-				
-//				AlertaErro(data);
-				console.log(data);
+					setTimeout(function () {
+						$(div).find('img').attr('src', '../../assets/icons/white-heart.svg');
+						$(div).removeClass('like');
+						$(div).removeClass('dislike');
+					}, 3000);
+				} else {
+					AlertaErro(data);
+				}
+			},
+			error: function (data) {
+				AlertaErro(data);
 			}
-		},
-		error: function (data) {
-			AlertaErro(data);
-			console.log(data);
-		}
-	});
-}
+		});
+	} else {
+		$.ajax({
+			url: '/PrettyStyle/controller.do?path=favorites&command=DeleteFavorite&json=true&id_user=' + idUser + '&token=' + userToken + '&id_product=' + id, type: 'POST',
+			success: function (data) {
+				if (data.success) {
+					AlertaSucesso(data);
+	
+					setTimeout(function () {
+						$(div).find('img').attr('src', '../../assets/icons/heart.svg');
+						$(div).removeClass('like');
+						$(div).removeClass('dislike');
+					}, 3000);
+				} else {
+					AlertaErro(data);
+				}
+			},
+			error: function (data) {
+				AlertaErro(data);
+			}
+		});
+	}
+};
 	
