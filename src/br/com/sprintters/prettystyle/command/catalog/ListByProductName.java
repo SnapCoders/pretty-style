c8e1f316e4ad2488a7cba85fb2b8ccfa3f668cb1
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-05-10 21:06:51 -0300

    change: desenvolvendo a funcionalidade de pesquisa de produtos

--- src/br/com/sprintters/prettystyle/command/catalog/ListByProductName.java
+++ src/br/com/sprintters/prettystyle/command/catalog/ListByProductName.java
@@ -11,8 +11,10 @@ import javax.servlet.http.HttpSession;
 import com.google.gson.Gson;
 
 import br.com.sprintters.prettystyle.command.Command;
+import br.com.sprintters.prettystyle.model.Category;
 import br.com.sprintters.prettystyle.model.Product;
 import br.com.sprintters.prettystyle.model.generic.Json;
+import br.com.sprintters.prettystyle.service.CategoryService;
 import br.com.sprintters.prettystyle.service.ProductService;
 
 public class ListByProductName implements Command {
@@ -24,10 +26,13 @@ public class ListByProductName implements Command {
 			boolean isJson = Boolean.parseBoolean(request.getParameter("json"));
 			
 			ProductService ps = new ProductService();
+			CategoryService cs = new CategoryService();
+
 			
-			String filter = request.getParameter("filter");
+			String search = request.getParameter("search");
 			
-			ArrayList<Product> products = ps.findByName(filter);
+			ArrayList<Category> categories = cs.list();
+			ArrayList<Product> products = ps.findByName(search);
 			
 			if (isJson) {
 				Json json = new Json(true, "", products);
@@ -36,7 +41,8 @@ public class ListByProductName implements Command {
 				response.getWriter().write(new Gson().toJson(json).toString());
 			} else {
 				session.setAttribute("products", products);
-				
+				session.setAttribute("categories", categories);
+
 				response.sendRedirect("/PrettyStyle/App/pages/catalog/catalog.jsp");
 			}
 		} catch (Exception e) {
